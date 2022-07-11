b1ef842bf95655632d49110664ed9ca253b02773
Gustavo Tavares <gustavo600@gmail.com>
2022-07-10 21:41:09 -0300

    poc graphql

---
+++ src/test/java/com/gustavo600/pocgraphql/PocGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo600.pocgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
